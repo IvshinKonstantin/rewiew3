baffb9d032812bcf93b749ea2f8c049a599d98d4
IvshinKonstantin <ivshin1997@gmail.com>
2026-03-31 21:27:50 +0500

    Update GEMIMI_functions.h

--- GEMIMI_functions.h
+++ GEMIMI_functions.h
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
 #include <string>
 
-// Èñïîëüçóåì wstring äëÿ ïîääåðæêè êèðèëëèöû
-void runShuffleProcess(const std::wstring& word);
+// Перемешивает строку, пока она снова не совпадет с оригиналом.
+void run_shuffle_process(const std::wstring& word);
+
+#endif  // FUNCTIONS_H_
